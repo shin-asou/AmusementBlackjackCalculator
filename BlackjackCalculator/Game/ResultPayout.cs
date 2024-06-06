33aa321e7431c4c8c602804d13e771478ef73683
shin-asou <shin.asou0120@gmail.com>
2024-06-06 21:16:31 +0900

    ルールを管理するクラスを追加

---
+++ BlackjackCalculator/Game/ResultPayout.cs
@@ -0,0 +1,10 @@
+﻿namespace BlackjackCalculator.Game
+{
+    public readonly struct ResultPayout(bool valid, decimal multiplier, decimal bonus)
+    {
+        public bool Valid { get; } = valid;
+        // payoutは元手もありの入力とする　(等倍配当を2.0とみなす)
+        public decimal Multiplier { get; } = multiplier;
+        public decimal Bonus { get; } = bonus;
+    }
+}
---
+++ BlackjackCalculator/Game/RuleSet.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Frozen;
+
+namespace BlackjackCalculator.Game
+{
+    // 標準的なBlackjackのルールを定義する
+    public class RuleSet(SurrenderType surrenderType, FrozenDictionary<GameResult, ResultPayout> payoutTable)
+    {
+        public SurrenderType Surrender { get; } = surrenderType;
+        public FrozenDictionary<GameResult, ResultPayout> ResultPayout { get; } = payoutTable;
+    }
+}
