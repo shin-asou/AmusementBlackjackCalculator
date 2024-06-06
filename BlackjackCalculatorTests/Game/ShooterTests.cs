72fddf58f7805f1bbd253d694f3eed9fd7e3e22f
shin-asou <shin.asou0120@gmail.com>
2024-06-06 23:23:38 +0900

    shooter.Pullでカードが違うものが引けているかのテストを追加 (#5)

--- BlackjackCalculatorTests/Game/ShooterTests.cs
+++ BlackjackCalculatorTests/Game/ShooterTests.cs
@@ -18,12 +18,15 @@ namespace BlackjackCalculator.Game.Tests
         {
             var shooter = ShooterFactory.BuildShooter(2, 1);
             var startCount = shooter.Count;
+            var beforeCard = Card.Ace;
             for (int i = 0; i < 50; i++)
             {
                 var result = shooter.Pull();
                 var nowCount = shooter.Count;
                 Assert.IsNotNull(result);
                 Assert.AreEqual(i + 1, startCount - nowCount);
+                if (i != 0) { Assert.IsFalse(Card.ReferenceEquals(result, beforeCard)); }
+                beforeCard = result;
             }
         }
     }
