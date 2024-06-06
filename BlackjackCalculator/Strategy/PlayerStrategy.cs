33abd3478dfa251f8331b5b3c00357f2f0ad7ae8
shin-asou <shin.asou0120@gmail.com>
2024-06-07 02:07:32 +0900

    リファクタリング+ハードハンドストラテジーが大きく間違っていたので修正

--- BlackjackCalculator/Strategy/PlayerStrategy.cs
+++ BlackjackCalculator/Strategy/PlayerStrategy.cs
@@ -5,7 +5,7 @@ namespace BlackjackCalculator.Strategy
     public abstract class PlayerStrategy(Hand hand) : AbstractStrategy(hand)
     {
         // Player は必ずupCardを知っているためupCardを取らないメソッドをサポートしない
-        public override HandAction Action() { throw new NotSupportedException(); }
+        public override HandAction Action() => throw new NotSupportedException();
         public override HandResult Result()
         {
             if (Hand.IsBlackjack) return HandResult.Blackjack;
