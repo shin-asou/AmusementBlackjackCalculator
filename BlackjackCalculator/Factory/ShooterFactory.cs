00896e05bced07d7269adfa224306addbb2a2426
shin-asou <shin.asou0120@gmail.com>
2024-06-06 22:57:44 +0900

    Shooterからカードを引けるようにした

---
+++ BlackjackCalculator/Factory/ShooterFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlackjackCalculator.Game;
+
+namespace BlackjackCalculator.Factory
+{
+    public static class ShooterFactory
+    {
+        public static Shooter BuildShooter(int deckCount, int useDeckCount) => new (deckCount, useDeckCount);
+    }
+}
