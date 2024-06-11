633bb5acd05b0cf96e789ae2b10ee2530363e1f7
shin-asou <shin.asou0120@gmail.com>
2024-06-12 01:37:19 +0900

    Shooter作成をBuildメソッドへ移行

--- BlackjackCalculator/Factory/ShooterFactory.cs
+++ BlackjackCalculator/Factory/ShooterFactory.cs
@@ -4,6 +4,11 @@ namespace BlackjackCalculator.Factory
 {
     public static class ShooterFactory
     {
-        public static Shooter BuildShooter(int deckCount, int useDeckCount) => new(deckCount, useDeckCount);
+        public static Shooter BuildShooter(int deckCount, int useDeckCount)
+        {
+            var result = new Shooter(deckCount, useDeckCount);
+            result.Build();
+            return result;
+        }
     }
 }
